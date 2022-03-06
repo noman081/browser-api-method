fbc2367fd3c20f43341da223387537ba4c185aa4
noman081 <abdullahalnoman642@gmail.com>
2022-03-07 00:58:18 +0600

    After added in local stroage

--- js/app.js
+++ js/app.js
@@ -2,12 +2,19 @@ const addCart = () => {
     const searchFeild = document.getElementById('search-text');
     const searchText = searchFeild.value;
     searchFeild.value = '';
-
+    if (!searchText) return;
     //display products in UI
     displayProducts(searchText);
 
     //add to local storage
-    addToCart(searchText);
+    addProductToCart(searchText);
+}
+const displayInitialCart = () => {
+    const cart = getCart();
+    console.log(cart);
+    for (const name in cart) {
+        displayProducts(name);
+    }
 }
 
 const displayProducts = productName => {
@@ -28,12 +35,17 @@ const getCart = () => {
     return cartObject;
 }
 
-const addToCart = name => {
+const addProductToCart = name => {
     const cart = getCart();
     cart[name] = 1;
     console.log(cart);
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified);
 }
 
 const purchaseNow = () => {
     document.getElementById('products').textContent = '';
+    localStorage.removeItem('cart');
 }
+
+displayInitialCart();
